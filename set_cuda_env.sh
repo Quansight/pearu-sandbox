f586fd533282fb4d3db49c153389234d32fe445c
Pearu Peterson <pearu.peterson@gmail.com>
2019-10-25 11:57:36 -0500

    Created set_cuda_env.sh

---
+++ set_cuda_env.sh
@@ -0,0 +1,37 @@
+#
+# Set CUDA environment.
+#
+# Installation:
+#   ln -s /path/to/cuda_set_env.sh /path/to/cuda/home/env.sh
+# Usage:
+#   source <this file>
+#
+# Author: Pearu Peterson
+# Created: October 2019
+#
+
+#echo "This is ${BASH_SOURCE[0]}"
+
+# Restore previous environment
+if [ ! -z "${CUDA_ENV_SH_CUDA_HOME_BACKUP+x}" ]
+then
+  # Restore previous environment:
+  export CUDA_HOME=${CUDA_ENV_SH_CUDA_HOME_BACKUP}
+  export PATH=${CUDA_ENV_SH_PATH_BACKUP}
+  export LD_LIBRARY_PATH=${CUDA_ENV_SH_LD_LIBRARY_PATH_BACKUP}
+fi
+
+# Backup current environment
+export CUDA_ENV_SH_CUDA_HOME_BACKUP=${CUDA_HOME}
+export CUDA_ENV_SH_PATH_BACKUP=${PATH}
+export CUDA_ENV_SH_LD_LIBRARY_PATH_BACKUP=${LD_LIBRARY_PATH}
+
+# Set new environment
+export CUDA_HOME=$(dirname ${BASH_SOURCE[0]})
+export PATH=${CUDA_HOME}/bin:${PATH}
+export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
+
+# Show new environment
+#echo "CUDA_HOME=${CUDA_HOME}"
+#echo "PATH=${PATH}"
+#echo "LD_LIBRARY_PATH=${LD_LIBRARY_PATH}"
